bb50e21e5c9f3c37b3967a3c49ea45460a91e6f0
Nikita Chaika <iasastudent2021@gmail.com>
2023-03-02 23:42:41 +0200

    final version

--- script.js
+++ script.js
@@ -53,10 +53,6 @@ class Cycling extends Workout {
     }
 }
 
-// const run1 = new Running([39,-12], 5.2,24,178);
-// const cycling1 = new Cycling([39,-12],27,95, 523);
-// console.log(run1, cycling1);
-
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
